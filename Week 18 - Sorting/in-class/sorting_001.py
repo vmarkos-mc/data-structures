a2978c29e29b087cdf1739af77d9fcd02ff59648
vmarkos-mc <v.markos@mc-class.gr>
2026-03-18 11:23:37 +0200

    Add W19 in-class h1

--- Week 18 - Sorting/in-class/sorting_001.py
+++ Week 18 - Sorting/in-class/sorting_001.py
@@ -1,13 +1,26 @@
 # in-class/sorting_001.py
 
-def bubble_sort(ns) -> list:
+def bubble_sort(ns, key = lambda x, y: x >= y) -> list:
     """Implements bubble sort, returning a new list"""
-    ...
+    sorted_ns = [x for x in ns] # To make sure we do not mutate `ns`
+    swapped = True # Just to make sure we enter the while loop
+    n = len(sorted_ns)
+    while swapped and n > 1:
+        swapped = False
+        for i in range(1, n):
+            if not key(sorted_ns[i], sorted_ns[i - 1]):
+                sorted_ns[i - 1], sorted_ns[i] = sorted_ns[i], sorted_ns[i - 1]
+                swapped = True
+                # temp = sorted_ns[i]
+                # sorted_ns[i] = sorted_ns[i - 1]
+                # sorted_ns[i - 1] = temp
+        n -= 1
+    return sorted_ns
 
 
 def merge_sort(ns) -> list:
     """Implements merge sort, returning a new list"""
-    ...
+    return ns
 
 
 def list_to_str(ls) -> str:
